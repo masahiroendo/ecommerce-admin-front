a1b490817048cebcdc0fdb9c2ea988ccfbeab737
matcha <massahiro.endo@gmail.com>
2023-09-23 12:35:56 +0200

    feat: project init config
    install shadcn

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm">
+          &copy; 2023 Ecommerce Front Store, Inc. All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  );
+}
